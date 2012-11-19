94990cf63c1fac3dfa182b025be1bf596a41f5ad
renatodex <renatodex@gmail.com>
2012-11-19 18:19:13 -0200

    Conversao do Answer Key para inteiro

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,7 +1,7 @@
 var WebResult = require('./WebResult').model;
 
 exports.model = Entity.create({
-    process : function(Choices, Questions, Answers) {		
+    process : function(Choices, Questions, Answers) {	
         for(var i in Choices.store) {
             Choices.store[i].score = 0;
         }
@@ -9,12 +9,11 @@ exports.model = Entity.create({
         var question_obj;
         for(var k in Questions) {
             question_obj = Questions[k];
-            question_obj.answer(Answers[k]);	
+            question_obj.answer(parseInt(Answers[k]));	
         }
 
         var final_choice = Choices.getHigher().value;
         var graphic_data = Choices.get();
-        
         var result = WebResult.create();
         result.setFinalChoice(final_choice);
         result.setGraphicData(graphic_data);
